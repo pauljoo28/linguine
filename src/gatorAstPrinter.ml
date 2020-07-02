4c1d6212a767b0e8a63827f334bb6c0cc9d3eb84
pauljoo <pj93@cornell.edu>
2020-07-02 15:38:04 -0700

    Add in `:=` syntax - Beginning for development of requires - Currently acts only as a assignment operator

--- src/gatorAstPrinter.ml
+++ src/gatorAstPrinter.ml
@@ -99,7 +99,7 @@ and string_of_comm (c : comm) : string =
   | Skip -> "skip;"
   | Print e -> "print " ^ string_of_aexp e ^ ";"
   | Exp e -> string_of_aexp e ^ ";"
-  | Decl (ml, t, s, e) ->
+  | Decl (b, ml, t, s, e) ->
       string_of_mod_list ml ^ string_of_typ t ^ " " ^ s ^ " = "
       ^ string_of_aexp e ^ ";"
   | Assign (b, x) -> string_of_aexp b ^ " = " ^ string_of_aexp x ^ ";"
